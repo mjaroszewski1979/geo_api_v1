9dd12993e4402b1eb7e0d0812e03b31ca4cca0cc
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-12-16 18:38:33 +0100

    added procfile and requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn geo_api.wsgi:application
--- geo/utilities.py
+++ geo/utilities.py
@@ -1,9 +1,10 @@
+import os
 import requests
 from .models import Geolocation
 
 def get_geo(ip):
     fields = 'longitude,latitude,ip'
-    response = requests.get('http://api.ipstack.com/' + ip + '?access_key=cce8aa93f5ee3af0f4698023b2bc1f13&fields=' + fields)
+    response = requests.get('http://api.ipstack.com/' + ip + '?access_key=' + os.environ.get('API_KEY') + '&fields=' + fields)
     if response.status_code == 200:
         try:
             response_json = response.json()
