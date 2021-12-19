4db216eab731a22df93716e04e647fe9e0f06d66
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-12-19 20:10:29 +0100

    Update tests_unittest.py

--- tests/tests_unittest.py
+++ tests/tests_unittest.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # python imports
 import unittest
 
@@ -28,25 +27,3 @@ class TestGetGeo(unittest.TestCase):
                     'type': 'invalid_access_key',
                     'info': 'You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]'}}
         self.assertEquals(actual, expected)
-
-
-=======
-import unittest
-
-from geo.utilities import get_geo
-
-
-class TestGetGeo(unittest.TestCase):
-
-    def test_get_geo_valid_ip(self):
-        actual = get_geo('134.201.250.155')
-        expected = 'Geolocation added!'
-        self.assertEquals(actual, expected)
-
-    def test_get_geo_invalid_ip(self):
-        actual = get_geo('12')
-        expected = { "Error" : "Invalid IP address" }
-        self.assertEquals(actual, expected)
-
-
->>>>>>> ee8166a3b8bf4aaa4a9a9f9975f0fd942f57854e
