78cf56056618c51963f2b0679d91b3333fceefc6
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-12-19 21:07:55 +0100

    Update settings.py

--- geo_api/settings.py
+++ geo_api/settings.py
@@ -126,5 +126,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',)
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES' : ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
