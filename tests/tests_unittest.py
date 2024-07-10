8a62ada83419f2105e0fc6e73a5a3749ba73da3c
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-10 17:35:14 +0200

    Update tests_unittest.py

--- tests/tests_unittest.py
+++ tests/tests_unittest.py
@@ -1,19 +1,30 @@
-# python imports
+# Import unittest module for defining unit tests
 import unittest
+# Import os module for environment variables handling
 import os
 
-# app imports
+# Import get_geo function from 'geo.utilities'
 from geo.utilities import get_geo
 
-# Testing get_geo function
 class TestGetGeo(unittest.TestCase):
+    """
+    Test case for testing the get_geo function.
+    """
 
     def test_get_geo_valid_ip(self):
+        """
+        Test method for testing get_geo function with a valid IP address and API key.
+        """
+        
         actual = get_geo('134.201.250.155', os.environ.get('API_KEY'))
         expected = {'Success': 'Geolocation added!'}
         self.assertEquals(actual, expected)
 
     def test_get_geo_invalid_ip(self):
+        """
+        Test method for testing get_geo function with an invalid IP address and valid API key.
+        """
+        
         actual = get_geo('12',  os.environ.get('API_KEY'))
         expected = {'success': False,
                     'error': {'code': 106,
@@ -22,6 +33,10 @@ class TestGetGeo(unittest.TestCase):
         self.assertEquals(actual, expected)
 
     def test_get_geo_invalid_api_key(self):
+        """
+        Test method for testing get_geo function with a valid IP address and invalid API key.
+        """
+        
         actual = get_geo('134.201.250.155', 'invalid')
         expected = {'success': False,
                     'error': {'code': 101,
