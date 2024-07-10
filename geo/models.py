2db9dd2db1062ce7200df7fffcfd8af882293b66
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-10 10:31:43 +0200

    Update models.py

--- geo/models.py
+++ geo/models.py
@@ -1,11 +1,23 @@
+# Django imports for defining database models.
 from django.db import models
 
 
 class Geolocation(models.Model):
+    """
+    Model representing a geolocation entry with an IP address, longitude, and latitude.
+    """
+
+    # CharField for storing the IP address. This field is unique for each geolocation entry.
     ip = models.CharField(max_length=50, unique=True)
+    # CharField for storing the longitude of the geolocation.
     longitude = models.CharField(max_length=50)
+    # CharField for storing the latitude of the geolocation.
     latitude = models.CharField(max_length=50)
 
 
     def __str__(self):
+        """
+        Return a string representation of the geolocation entry.
+        This method returns the IP address of the geolocation.
+        """
         return self.ip
