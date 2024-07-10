47ca4aa8acd7c4926f25706c85bb642938407e1a
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-10 17:37:47 +0200

    Update apps.py

--- geo/apps.py
+++ geo/apps.py
@@ -1,6 +1,13 @@
+# Import AppConfig from Django's apps module
 from django.apps import AppConfig
 
 
 class GeoConfig(AppConfig):
+    """
+    Configuration class for the 'geo' Django app.
+    """
+    
+    # Define the default_auto_field for models
     default_auto_field = 'django.db.models.BigAutoField'
+    # Specify the name of the Django app 'geo'
     name = 'geo'
