295c379a6fc12f09ebc12e3411df73345a1b7e7a
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-10 10:44:10 +0200

    Update serializers.py

--- geo/serializers.py
+++ geo/serializers.py
@@ -1,13 +1,34 @@
+# Import serializers from Django REST framework
 from rest_framework import serializers
+# Import Geolocation model from the current application
 from .models import Geolocation
 
-# Creating serializers combined with Geolocation model
+# Define a serializer for the Geolocation model to handle only the IP field
 class GeoIpSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the Geolocation model, limited to the 'ip' field.
+    """
     class Meta:
+        """
+        Meta class to specify the model and fields to be serialized.
+        """
+
+        # Associate this serializer with the Geolocation model
         model = Geolocation
+        # Specify that only the 'ip' field should be included in the serialization
         fields = ('ip',)
 
+# Define a serializer for the Geolocation model to handle all fields
 class GeoSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the Geolocation model, including all fields.
+    """
     class Meta:
+        """
+        Meta class to specify the model and fields to be serialized.
+        """
+
+        # Associate this serializer with the Geolocation model
         model = Geolocation
+        # Include all fields of the Geolocation model in the serialization
         fields = '__all__'
